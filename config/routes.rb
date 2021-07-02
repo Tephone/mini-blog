eef85603e450c61d34b9ac5f688f2fb2e165497a
naoko kasahara <kassa0918@gmail.com>
2021-07-02 11:45:01 +0900

    プロフィールページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  # get "users/show", to: 'users#show'
+  resources :users, only: [:show, :edit]
   resources :blogs
   root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
